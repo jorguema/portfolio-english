203a2e49353a7abd3dfc5bcf4003a881253ccb52
Jorge Guerola <jorge.guerolamartnez@sage.com>
2024-04-19 14:01:31 +0200

    init setup

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function About() {
+    return (
+      <main id="main">
+        <h1>This is the ABOUT page</h1>
+        <Link to="/">GO TO HOME</Link>
+      </main>
+    );
+  }
+   export default About;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter, Link, useLocation } from "react-router-dom"
+import Home from './Home';
+import About from './About';
+import { useEffect } from 'react';
+import Navbar from './Navbar';
 
 function App() {
+  // const location = useLocation();
+
+  // useEffect(() => {
+  //   console.log('Location changed');
+  //   debugger;
+  // }, [location]);
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         hello react
-      </header>
+      </header> */}
+
+
+
+        <BrowserRouter>
+
+        <Navbar></Navbar>
+          <Routes>
+            {/* <Route element={<Layout />} > */}
+
+            <Route >
+              <Route exact path="/" element={<Home />} />
+              <Route path="about" element={<About />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
